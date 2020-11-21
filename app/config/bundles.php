18c6cfb8305ab7515ee1ba55f03f10c203559d19
Richard Melo <allucardster@gmail.com>
2020-11-21 00:13:40 -0500

    Add `friendsofsymfony/rest-bundle`

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Danaki\DoctrineEnumTypeBundle\DanakiDoctrineEnumTypeBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
